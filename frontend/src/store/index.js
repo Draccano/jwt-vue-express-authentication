80df244cd36bde9c5e315a8d5ebbd42e50af39a2
Dragino <dracossos@gmail.com>
2020-06-03 16:08:11 +0100

    vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,43 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      storage: window.sessionStorage,
+    }),
+  ],
+  strict: true,
   state: {
+    token: null,
+    user: null,
+    isUserLoggedIn: false,
   },
   mutations: {
+    setToken(state, token) {
+      state.token = token;
+      if (token) {
+        state.isUserLoggedIn = true;
+      } else {
+        state.isUserLoggedIn = false;
+      }
+    },
+    setUser(state, user) {
+      state.user = user;
+    },
   },
   actions: {
+    setToken({ commit }, token) {
+      //async action
+      commit('setToken', token);
+    },
+    setUser({ commit }, user) {
+      //async action
+      commit('setUser', user);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
