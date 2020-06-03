8ccfbdb67bbb0c6b02e368a3010c94d33c5069bc
Dragino <dracossos@gmail.com>
2020-06-03 16:08:36 +0100

    routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,31 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Register from '../components/Register.vue'
-import Login from '../components/Login.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Register from '../components/Register.vue';
+import Login from '../components/Login.vue';
+import Welcome from '../components/Welcome.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: Home,
   },
   {
     path: '/registration',
-    name: 'Register',
-    component: Register
+    name: 'register',
+    component: Register,
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: Login,
   },
-]
+  {
+    path: '/welcome',
+    name: 'welcome',
+    component: Welcome,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
