2c1d3bba07aac9c5e75431ac42f8628b934ba2a5
Dragino <dracossos@gmail.com>
2020-06-07 00:04:27 +0100

    update message route

--- backend/src/routes/message.js
+++ backend/src/routes/message.js
@@ -3,11 +3,16 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const isAuthenticated = require('../policies/isAuthenticated');
 require('dotenv/config');
 const MessageController = require('../controllers/MessageController');
 
-router.get('/:userId/messages', MessageController.index);
-router.post('/messages', MessageController.post);
-router.delete('/:userId/messages/:messageId', MessageController.delete);
+router.get('/messages', isAuthenticated, MessageController.index);
+router.post('/messages', isAuthenticated, MessageController.post);
+router.delete(
+  '/messages/:messageId',
+  isAuthenticated,
+  MessageController.delete
+);
 
 module.exports = router;
